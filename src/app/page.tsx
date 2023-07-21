af3311fb9d2b68bf42f61d44942da01878501468
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-21 19:22:31 -0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hello world!!</h1>
-      <h6>Formatação de condigo</h6>
+      <h6>Trabalhando com Github Actions</h6>
     </main>
   );
 }
