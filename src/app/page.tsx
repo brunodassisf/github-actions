17125e97aaf41936a34003293f4bf3b0cdda39d1
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-20 21:23:13 -0300

    Fix - run lint and fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<main>
-<h1>Hello world</h1>
-<h6>Formatação de condigo</h6>
-</main>
+    <main>
+      <h1>Hello world</h1>
+      <h6>Formatação de condigo</h6>
+    </main>
   );
 }
