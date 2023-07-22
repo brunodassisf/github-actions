eae0ae326eb894fb4837a716ab8fdba3915779f7
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-21 23:13:06 -0300

    Preview deploy setup

---
+++ scripts/deploy-preview.js
@@ -0,0 +1,7 @@
+import { execSync } from "child_process";
+
+console.log("[DEPLOY_PREVIEW]: START...");
+const command = "yarn deploy:staging";
+const output = execSync(command, { encoding: "utf8" });
+console.log(output);
+console.log("[DEPLOY_PREVIEW]: END");
